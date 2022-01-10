bb75b2460053c073509949cf26e3a1f270a20170
yu <78803363+yujiah-github@users.noreply.github.com>
2022-01-11 00:36:49 +0900

    Add todolist_jpa

--- todolist_jpa/src/main/java/com/todolist/todolist_jpa/controller/ListController.java
+++ todolist_jpa/src/main/java/com/todolist/todolist_jpa/controller/ListController.java
@@ -1,41 +1,77 @@
+
+
 package com.todolist.todolist_jpa.controller;
 
 import com.todolist.todolist_jpa.domain.entity.ListEntity;
 import com.todolist.todolist_jpa.dto.ListDto;
 import com.todolist.todolist_jpa.service.ListServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/")
 public class ListController {
-
     private final ListServiceImpl listService;
 
     @GetMapping(path = "")
     public String index() {
+        List<ListEntity> lists = listService.searchAll();
         return "index";
     }
 
-    @PostMapping(path = "test")
-    public ResponseEntity<?> test(@RequestBody ListDto body) {
-        //save logic
-        //.......
-        ListDto result = ListDto.builder()
-            .listId(1L)
-            .listTitle("title")
-            .listCompleted(true)
-            .build();
+    @GetMapping(path = "/todolist") //전체 목록 불러오기
+    public ResponseEntity<List<ListEntity>> searchAll() {
+        List<ListEntity> result = listService.searchAll();
         return ResponseEntity.ok(result);
     }
+
+    @PostMapping(path = "/todo") //저장하기
+    public ResponseEntity<?> add(@RequestBody ListDto body) {
+        ListEntity result = listService.add(body);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(path = "/todo/{id}") //아이디로 찾아서 지우기
+    public ListEntity searchById(Long listId){
+        return this.listService.searchById(listId);
+    }
+
+    @DeleteMapping(path = "/todolist") //전체 삭제하기
+    public void deleteAll() {
+        this.listService.deleteAll();
+    }
+
+    @DeleteMapping(path = "/todo/{id}") //아이디로 삭제하기
+    public ResponseEntity<List<ListEntity>> deleteById(@PathVariable Long id) {
+        return ResponseEntity.ok(listService.deleteById(id));
+    }
+
+    @PatchMapping(path = "/todo/{id}") //아이디로 업데이트하기
+    public ResponseEntity<?> updateById(@PathVariable Long id, @RequestBody ListDto body){
+        List<ListEntity> result = listService.updateById(id, body);
+        return ResponseEntity.ok(result);
+    }
+
+
+  /*  @DeleteMapping(path = "deletetest")
+    public ResponseEntity<?> deletetest(@RequestBody ListDto body) {
+        ListDto result = ListDto.builder()
+                .listId(body.getListId())
+                .listTitle(body.getListTitle())
+                .listCompleted(body.isListCompleted())
+                .build();
+
+        listService.deleteAll(body);
+        return ResponseEntity.ok(result);
+    }
+*/
+
 
 //    @PostMapping
 //    public ResponseEntity<ListEntity> create(@RequestBody ListDto listDto) {
--- todolist_jpa/src/main/java/com/todolist/todolist_jpa/service/ListServiceImpl.java
+++ todolist_jpa/src/main/java/com/todolist/todolist_jpa/service/ListServiceImpl.java
@@ -3,17 +3,16 @@ package com.todolist.todolist_jpa.service;
 import com.todolist.todolist_jpa.domain.entity.ListEntity;
 import com.todolist.todolist_jpa.dto.ListDto;
 import com.todolist.todolist_jpa.repository.ListRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
-public class ListServiceImpl implements ListService {
+public class ListServiceImpl implements ListService{
 
 
     @Autowired
@@ -41,32 +40,29 @@ public class ListServiceImpl implements ListService {
     }
 
     @Override
-    public ListEntity updateById(Long listId, ListDto listDto) {
-        ListEntity listEntity = this.searchById(listId);
+    public List<ListEntity> updateById(Long listId, ListDto listDto) {
+        ListEntity todo = ListEntity.builder()
+                .listId(listId)
+                .listTitle(listDto.getListTitle())
+                .listCompleted(listDto.isListCompleted())
+                .build();
+        this.listRepository.save(todo);
+        return this.listRepository.findAll();
 
-        if (listDto.getListId() != null) {
-            listEntity.setListId(listDto.getListId());
-        }
 
-        if (listDto.getListTitle() != null) {
-            listEntity.setListTitle(listDto.getListTitle());
-        }
-
-        if (listDto.isListCompleted()) {
-            listEntity.setListCompleted(listDto.isListCompleted());
-        }
-
-        return this.listRepository.save(listEntity);
     }
 
     @Override
-    public void deleteById(Long listId) {
+    @Transactional
+    public List<ListEntity> deleteById(Long listId){
         this.listRepository.deleteById(listId);
+        return listRepository.findAll();
     }
 
     @Override
     public void deleteAll() {
         this.listRepository.deleteAll();
-
     }
+
 }
+
