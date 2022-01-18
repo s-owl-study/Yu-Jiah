8c4772db9c5a57b92d34ed969b0829cf73bda8d0
yu <78803363+yujiah-github@users.noreply.github.com>
2022-01-18 22:57:50 +0900

    delete view

--- 게시판 만들기/Board/src/main/java/com/example/board/repository/AttachedFilesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.AttachedFilesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachedFilesRepository extends JpaRepository<AttachedFilesEntity, Long> {
-}
--- 게시판 만들기/Board/src/main/java/com/example/board/repository/RecommendationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.RecommendationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendationRepository extends JpaRepository<RecommendationEntity, Long> {
-}
